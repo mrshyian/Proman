b8f939670dfb586862828f88957fa7fec35a8fe5
Tetiana Shcherbak <shcherbak.tet@gmail.com>
2022-01-25 21:53:21 +0100

    Refactor: loadCards (cardsManager.js)

--- static/js/controller/cardsManager.js
+++ static/js/controller/cardsManager.js
@@ -4,20 +4,13 @@ import { domManager } from "../view/domManager.js";
 
 export let cardsManager = {
  loadCards: async function (boardId) {
-    const cards = await dataHandler.getCardsByBoardId(boardId);
-    const boardComponents = document.querySelector(`.board[data-board-id="${boardId}"]`);
-    const cardsOnBoard = boardComponents.getElementsByClassName("card");
+    const cardsFromBase = await dataHandler.getCardsByBoardId(boardId);
+    const renderedCardIds = getRenderedCardIds(boardId);
 
-    const existCardIds = new Set();
-
-    for(const card of cardsOnBoard){
-        const cardId = +card.dataset.cardId;
-        existCardIds.add(cardId);
-    }
-
-    for (let card of cards.filter(card => !existCardIds.has(card.id))) {
+    for (let card of cardsFromBase.filter(card => !renderedCardIds.has(card.id))) {
       const cardBuilder = htmlFactory(htmlTemplates.card);
       const content = cardBuilder(card);
+
       domManager.addChild(`.board[data-board-id="${boardId}"]`, content);
       domManager.addEventListener(
         `.card[data-card-id="${card.id}"]`,
@@ -30,4 +23,23 @@ export let cardsManager = {
 
 function deleteButtonHandler(clickEvent) {
   clickEvent.target.remove();
+}
+
+function getRenderedCardIds(boardId){
+    const cardsOnBoard = getCardsOnBoard(boardId);
+    const existCardIds = new Set();
+
+    for(const card of cardsOnBoard){
+        const cardId = +card.dataset.cardId;
+        existCardIds.add(cardId);
+    }
+
+    return existCardIds;
+}
+
+function getCardsOnBoard(boardId){
+    const boardComponents = document.querySelector(`.board[data-board-id="${boardId}"]`);
+    const cardsOnBoard = boardComponents.getElementsByClassName("card");
+
+    return cardsOnBoard;
 }
