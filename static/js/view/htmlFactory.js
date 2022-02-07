f42e588b96954f535cc86b9371b1ce3761d26f0e
Tetiana Shcherbak <shcherbak.tet@gmail.com>
2022-02-07 19:05:39 +0100

    FIXED: column width changes dynamically

--- static/js/view/htmlFactory.js
+++ static/js/view/htmlFactory.js
@@ -40,10 +40,8 @@ export async function statusColumnsBuilder(){
     const amountOfColumns = cardStatuses.length;
 
     for (const status of cardStatuses){
-        // const div = `<div class="status-column" data-status-id="${status.id}" style="width: calc(100%/${amountOfColumns}); text-align: center; vertical-align: middle;"> ${status['title']}</div>`;
-        //const div = `<div class="board-column" data-status-id="${status.id}">${status['title']}</div>`;
 
-        const div = `<div class="board-column" data-status-id="${status.id}">
+        const div = `<div class="board-column" data-status-id="${status.id} style="width: calc(100%/${amountOfColumns});">
             <div class="board-column-title" data-status-id="${status.id}">${status['title']}</div>
             <div class="board-column-content drop-zone" data-status-id="${status.id}" ></div>
         </div>`;
@@ -53,9 +51,7 @@ export async function statusColumnsBuilder(){
 }
 
 function cardBuilder(card) {
-    //return `<div class="card" data-card-id="${card.id}" style="width: 90%; text-align: center; display: block; float:right"><span class="card-title">${card.title}</span><span class="card-remove" style='float: right; text-align: center; width: 20px; cursor: pointer; background-color: lightgray;' id="${card.id}-span">x</span></div>`;
-    //return `<div class="card draggable" data-card-id="${card.id}" draggable="true" style="width: 90%; text-align: center; display: block; float:right">${card.title}<span style='float: right; text-align: center; width: 20px; cursor: pointer; background-color: lightgray;' id="${card.id}-span">x</span></div>`;
-    return `<div class="card draggable" data-card-id="${card.id}" draggable="true" style="width: 90%; text-align: center; display: block; float:right">
+     return `<div class="card draggable" data-card-id="${card.id}" draggable="true" style="width: 90%; text-align: center; display: block; float:right">
         <span class="card-title">${card.title}</span>
         <span class="card-remove" style='float: right; text-align: center; width: 20px; cursor: pointer; background-color: lightgray;'id="${card.id}-span">x</span>
         </div>`
