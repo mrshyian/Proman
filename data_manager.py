f7092e3d85faec3a5a9a7252cff8863d3a29d636
Tetiana Shcherbak <shcherbak.tet@gmail.com>
2022-02-08 23:49:56 +0100

    data manager

--- data_manager.py
+++ data_manager.py
@@ -12,15 +12,15 @@ def establish_connection(connection_data=None):
     if connection_data is None:
         connection_data = get_connection_data()
     try:
-        # connect_str = "dbname={} user={} host={} password={} port={}".format(connection_data['dbname'],
-        #                                                                      connection_data['user'],
-        #                                                                      connection_data['host'],
-        #                                                                      connection_data['password'],
-        #                                                                      connection_data['port'])
-        connect_str = "dbname={} user={} host={} password={}".format(connection_data['dbname'],
-                                                                     connection_data['user'],
-                                                                     connection_data['host'],
-                                                                     connection_data['password'])
+        connect_str = "dbname={} user={} host={} password={} port={}".format(connection_data['dbname'],
+                                                                             connection_data['user'],
+                                                                             connection_data['host'],
+                                                                             connection_data['password'],
+                                                                             connection_data['port'])
+        # connect_str = "dbname={} user={} host={} password={}".format(connection_data['dbname'],
+        #                                                              connection_data['user'],
+        #                                                              connection_data['host'],
+        #                                                              connection_data['password'])
 
         conn = psycopg2.connect(connect_str)
         conn.autocommit = True
@@ -40,19 +40,19 @@ def get_connection_data(db_name=None):
     if db_name is None:
         db_name = os.environ.get('MY_PSQL_DBNAME')
 
-    # return {
-    #     'dbname': 'proman',
-    #     'user': 'postgres',
-    #     'host': 'localhost',
-    #     'password': 'postgres',
-    #     'port': 5433
-    # }
     return {
-        'dbname': db_name,
-        'user': os.environ.get('MY_PSQL_USER'),
-        'host': os.environ.get('MY_PSQL_HOST'),
-        'password': os.environ.get('MY_PSQL_PASSWORD')
+        'dbname': 'proman',
+        'user': 'postgres',
+        'host': 'localhost',
+        'password': 'postgres',
+        'port': 5433
     }
+    # return {
+    #     'dbname': db_name,
+    #     'user': os.environ.get('MY_PSQL_USER'),
+    #     'host': os.environ.get('MY_PSQL_HOST'),
+    #     'password': os.environ.get('MY_PSQL_PASSWORD')
+    # }
 
 
 def execute_select(statement, variables=None, fetchall=True):
