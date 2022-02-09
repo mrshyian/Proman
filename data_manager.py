a7ae9dd38a05d442bc686db82411a276ed9ea315
mrshyian <shyvlad1994@gmail.com>
2022-02-09 16:21:28 +0100

    little update

--- data_manager.py
+++ data_manager.py
@@ -9,6 +9,7 @@ def establish_connection(connection_data=None):
     :connection_data: Connection string attributes
     :returns: psycopg2.connection
     """
+
     if connection_data is None:
         connection_data = get_connection_data()
     try:
