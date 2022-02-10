3cfa229dfc0a05430eaf08738733cd70129f4fa7
Grzegorz Dąbrowski <g.dabrowski@onekk.eu>
2022-02-10 20:24:14 +0100

    ui: add styles for board buttons (in progress)

--- static/js/controller/boardsManager.js
+++ static/js/controller/boardsManager.js
@@ -17,25 +17,28 @@ export let boardsManager = {
 
 
 async function showHideButtonHandler(clickEvent) {
-    const buttonName = clickEvent.target.textContent;
-    const boardId = clickEvent.target.dataset.boardId;
-    if (buttonName === 'Show Cards') {
+    const buttonName = clickEvent.target.parentElement.dataset.btnName;
+    const boardId = clickEvent.target.parentElement.dataset.boardId;
+    if (buttonName === 'show') {
         const statusContent = await statusColumnsBuilder(boardId);
         domManager.addChild(`.board-columns[data-board-id="${boardId}"]`, statusContent);
         await cardsManager.loadCards(boardId);
         await addEventOnAllColumns(boardId);
-        clickEvent.target.innerHTML = 'Hide Cards'
-    } else if (buttonName === 'Hide Cards') {
-        clickEvent.target.innerHTML = 'Show Cards'
+        clickEvent.target.parentElement.setAttribute('data-btn-name','hide');
+        clickEvent.target.parentElement.innerHTML = '<i class="material-icons-outlined">expand_less</i>';
+    } else if (buttonName === 'hide') {
+        clickEvent.target.parentElement.setAttribute('data-btn-name','show');
+        clickEvent.target.parentElement.innerHTML = '<i class="material-icons-outlined">expand_more</i>';
         await cardsManager.hideCards(boardId);
     }
 }
 
 async function deleteBoard(clickEvent) {
-    const boardId = clickEvent.target.dataset.boardId;
-    clickEvent.target.parentElement.parentElement.parentElement.remove();
+    const boardId = clickEvent.target.parentElement.dataset.boardId;
+    clickEvent.target.parentElement.parentElement.parentElement.parentElement.remove();
     await dataHandler.deleteAnyBoard(boardId);
 }
+
 
 function addButtonNewBoard() {
     const content = newBoardButtonBuilder()
@@ -111,7 +114,7 @@ function activateRenameBoardModal(boardId) {
 
 
 async function addCard(clickEvent) {
-    const boardId = clickEvent.target.dataset.boardId;
+    const boardId = clickEvent.target.parentElement.dataset.boardId;
     const card = await dataHandler.createNewCard(boardId);
     const cardStatusId = card["status_id"];
     const cardBuilder = htmlFactory(htmlTemplates.card);
@@ -134,7 +137,7 @@ async function addCard(clickEvent) {
 
 
 async function createNewColumn(clickEvent) {
-    const boardId = clickEvent.target.dataset.boardId;
+    const boardId = clickEvent.target.parentElement.dataset.boardId;
     await dataHandler.createNewColumn(boardId);
     document.getElementById('root').innerHTML = ""
     addButtonNewBoard()
@@ -143,7 +146,7 @@ async function createNewColumn(clickEvent) {
         await createBoard(board);
     }
     const showHideBoardButton = document.querySelector(`.toggle-board-button[data-board-id="${boardId}"]`);
-    showHideBoardButton.click();
+    // showHideBoardButton.children[0].click();
 }
 
 
@@ -165,8 +168,9 @@ async function addEventOnAllColumns(boardId) {
 
 
 async function deleteColumn(clickEvent) {
-    const statusId = clickEvent.target.dataset.statusId;
-    clickEvent.target.parentNode.remove();
+    const statusId = clickEvent.target.parentElement.dataset.statusId;
+    console.log(statusId);
+    clickEvent.target.parentElement.parentElement.parentElement.parentElement.remove();
     await dataHandler.deleteAnyColumn(statusId);
 }
 
